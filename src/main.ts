129e07f76770baf89bb1279c32182885789042c3
Thiago-CSantos <thiagocarretero433@gmail.com>
2023-12-31 20:32:28 -0300

    feat: Configurações CORS globais para todas as rotas

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurações CORS globais para todas as rotas
+  const corsOptions: CorsOptions = {
+    origin: 'https://calm-teal-seagull-slip.cyclic.app',  // Altere para o domínio do seu cliente
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+
+  app.enableCors(corsOptions);
+
   await app.listen(3000);
   config();
 }
