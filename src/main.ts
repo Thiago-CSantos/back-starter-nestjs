2f4524b4f9edefb189a01e2f8bfd70c977d84b8a
Thiago-CSantos <thiagocarretero433@gmail.com>
2024-01-01 22:22:02 -0300

    enableCprs

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Configurações CORS globais para todas as rotas
-  const corsOptions: CorsOptions = {
-    origin: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: true,
-    optionsSuccessStatus: 204
-  };
+  // const corsOptions: CorsOptions = {
+  //   origin: true,
+  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  //   preflightContinue: true,
+  //   optionsSuccessStatus: 204
+  // };
 
-  app.enableCors(corsOptions);
+  app.enableCors();
 
   await app.listen(3000);
   config();
