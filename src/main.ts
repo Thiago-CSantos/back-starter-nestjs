07f84e4bd22fe8bdfea76509bc3b43c5b168798d
Thiago-CSantos <thiagocarretero433@gmail.com>
2024-01-02 09:11:48 -0300

    poi

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'dotenv';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Configurações CORS globais para todas as rotas
-  const corsOptions: CorsOptions = {
-    origin: 'http://localhost:3000',
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: true,
-    optionsSuccessStatus: 204
-  };
+  // const corsOptions: CorsOptions = {
+  //   origin: 'http://localhost:3000',
+  //   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  //   preflightContinue: true,
+  //   optionsSuccessStatus: 204
+  // };
 
-  app.enableCors(corsOptions);
+  app.enableCors();
 
   await app.listen(3000);
 }
